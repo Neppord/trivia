73b720aeb85d0429ef099be18dc19f259e03f439
neppord <neppord@gmail.com>
2022-06-07 15:54:16 +0200

    Create patch function and test adding of player

--- nodejs/src/game.spec.js
+++ nodejs/src/game.spec.js
@@ -3,7 +3,7 @@ const Game = require('./game.js');
 
 describe("The test environment", function () {
   it("should pass", function () {
-    (true).should.equal(true);
+    true.should.equal(true);
   });
 
   it("should access game", function () {
@@ -11,6 +11,21 @@ describe("The test environment", function () {
   });
 });
 
-describe("Your specs...", function () {
-  // it ...
+function patch(on, prop, with_, while_) {
+  let real = on[prop]
+  on[prop] = with_
+  let ret = while_()
+  on[prop] = real
+  return ret
+}
+
+describe("Game", function () {
+  it("logs when adding players", () => {
+    let spyLog = []
+    patch(console, "log", spyLog.push.bind(spyLog), () => {
+      let game = new Game()
+      game.add("<My Name>")
+      Should(spyLog).eql(['<My Name> was added', 'They are player number 1'])
+    })
+  })
 });
